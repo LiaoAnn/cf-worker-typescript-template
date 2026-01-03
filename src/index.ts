0a188cffcf1a70ec6cbcefdf08214bab5544d58a
LiaoAnn <1050123tom@gmail.com>
2026-01-03 08:03:21 +0000

    chore: name generated workers type as Env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono<{ Bindings: Env }>();
 
 app.route('/ping', ping);
 
